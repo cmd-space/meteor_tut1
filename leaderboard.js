97855ec693a9628f8898ce240d4fba40d1dbd635
Mason Crane <masonecrane@gmail.com>
2015-08-30 17:43:46 -0600

    added in some database functionality to alter the score of each player, and sort the list in descending order

--- leaderboard.js
+++ leaderboard.js
@@ -3,13 +3,29 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient) {
     Template.leaderboard.helpers ({
         'player': function() {
-            return PlayersList.find();
+            return PlayersList.find({}, {sort: {score: -1}});
+        },
+        'selectedClass': function() {
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if(playerId == selectedPlayer) {
+                return "selected";
+            }
         }
     });
 
     Template.leaderboard.events ({
         'click .player': function() {
-            console.log('You clicked a .player element');
+            var playerId = this._id;
+            Session.set('selectedPlayer', playerId);
+        },
+        'click .increment': function() {
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+        },
+        'click .decrement': function() {
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: -5}});
         }
     });
 }
